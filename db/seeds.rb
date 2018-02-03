3dee1600080057080b3bc1f0b49ed3190b6c24d3
notadeejay <amy@lighthouselabs.ca>
2018-02-03 15:37:07 -0500

    Form capturing

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,3 +51,94 @@
       price: "$",
       cuisine: "sandwiches"
       })
+
+    Restaurant.create!({
+      name: "Forno Cultura", 
+      description: "an alternative bakery space based in quality and tradition",
+      website: "http://www.fornocultura.com",
+      address: "609 King St W, Toronto, ON",
+      price: "$$",
+      cuisine: "sandwiches"
+      })
+    
+    Restaurant.create!({
+      name: "CJ lunchbox", 
+      description: "no frills spot for cheap sushi",
+      website: "https://www.yelp.ca/biz/cj-lunch-box-toronto",
+      address: "409 Richmond St W, Toronto, ON",
+      price: "$",
+      cuisine: "sushi"
+      })
+
+    Restaurant.create!({
+      name: "Pokito", 
+      description: "poké dishes made only from the freshest ingredients",
+      website: "https://www.pokito.ca",
+      address: "420 Queen St W, Toronto, ON",
+      price: "$$",
+      cuisine: "poke"
+      })
+
+    Restaurant.create!({
+      name: "The Chickery", 
+      description: "premium roast chicken and sides made with real ingredients and unique seasonings",
+      website: "https://www.thechickery.com",
+      address: "130 Spadina Ave, Toronto, ON",
+      price: "$$",
+      cuisine: "chicken"
+      })
+  
+    Restaurant.create!({
+      name: "Big Smoke Burger", 
+      description: "serving only 100% Canadian AAA beef, fresh lamb + chicken with hand-cut fries daily + poutine, homemade sauces + milkshakes",
+      website: "https://www.bigsmokeburger.com",
+      address: "573 King St W, Toronto, ON",
+      price: "$$",
+      cuisine: "burgers"
+      })
+
+    Restaurant.create!({
+      name: "Thai Princess", 
+      description: "Snug choice with simple decor that serves classic Thai dishes & fixed-price lunch specials.",
+      website: "https://www.thaiprincess.ca",
+      address: "387 King St W, Toronto, ON",
+      price: "$",
+      cuisine: "thai"
+      })
+   
+    Restaurant.create!({
+      name: "Fresh", 
+      description: "A large menu of modern vegetarian & vegan dishes, plus made-to-order juices, in a lively eatery",
+      website: "https://www.freshrestaurants.ca",
+      address: "147 Spadina Ave, Toronto, ON",
+      price: "$$",
+      cuisine: "vegetarian"
+    })
+
+    Restaurant.create!({
+      name: "Freshii", 
+      description: "Health-minded, counter-serve chain offering tossed-to-order salads plus soups, burritos & more",
+      website: "https://www.freshii.com",
+      address: "140 Spadina Ave, Toronto, ON",
+      price: "$",
+      cuisine: "healthy"
+    })
+
+
+    Restaurant.create!({
+      name: "Buna's Kitchen", 
+      description: "Hearty comfort food (like the kind grandma makes) is the specialty at this modern quick-serve cafe.",
+      website: "https://www.bunas.kitchen",
+      address: "388 Richmond St W, Toronto, ON",
+      price: "$$",
+      cuisine: "cafe"
+    })
+  
+    Restaurant.create!({
+      name: "Basil Box", 
+      description: "Authentic grab and go inspired by the bustling streets of Southeast Asia.",
+      website: "https://www.thebasilbox.com",
+      address: "411 Queen St W, Toronto, ON",
+      price: "$$",
+      cuisine: "asian"
+    })
